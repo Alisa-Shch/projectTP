eed195d013cdfa8f27d60df053a664996600c962
Kiradey <alex.sura143@gmail.com>
2024-09-16 18:41:05 +0300

    workflowStep

---
+++ app/app/WorkflowStep.cs
@@ -0,0 +1,14 @@
+﻿namespace app
+{
+    internal class WorkflowStep
+    {
+        public string Status { get; private set; }
+        public string Feedback { get; private set; }
+        public string Description { get; private set; }
+        public Guid RoleID { get; private set; }
+        public Guid EmployeeID { get; private set; }
+        public DateTime ModifiedDate { get; private set; }
+        public int NumberStep { get; private set; }
+
+    }
+}
---
+++ app/app/WorkflowTemplateStep.cs
@@ -0,0 +1,10 @@
+﻿namespace app
+{
+    internal class WorkflowTemplateStep
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Guid EmployeeID { get; private set; }
+        public Guid RoleID { get; private set; }
+    }
+}
