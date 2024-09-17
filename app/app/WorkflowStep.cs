e70c2b0996d29ca75c5d7d224c6bd92d070afce3
Kiradey <alex.sura143@gmail.com>
2024-09-17 16:18:42 +0300

    add

--- app/app/WorkflowStep.cs
+++ app/app/WorkflowStep.cs
@@ -10,5 +10,24 @@
         public DateTime ModifiedDate { get; private set; }
         public int NumberStep { get; private set; }
 
+        WorkflowStep(Guid roleID, string description)
+        {
+            Status = Position.InProgress;
+            Description = description;
+            RoleID = roleID;
+            ModifiedDate = DateTime.Now;
+        }
+
+        public void Approve(Employers employer, string feedback)
+        {
+            Workflow.Approve(employer, feedback);
+            WorkflowTemplate.Update();
+        }
+
+        public void Reject(Employers employer, string feedback)
+        {
+            Workflow.Reject(employer, feedback);
+            WorkflowTemplate.Update();
+        }
     }
 }
