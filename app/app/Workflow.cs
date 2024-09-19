56b6d5236e9b8bd1b99dd0c8ff9ed6d6a556b9c7
Alisa-Shch <132129891+Alisa-Shch@users.noreply.github.com>
2024-09-19 17:39:48 +0300

    workflow (#2)

---
+++ app/app/Workflow.cs
@@ -0,0 +1,55 @@
+﻿namespace app
+{
+    internal class Workflow
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Guid TemplateID { get; private set; }
+        public Guid CandidateId { get; private set; }
+        public Guid InvitingEID { get; private set; }
+        public Position Status { get; private set; }
+        public Guid ID { get; private set; }
+
+        public List<WorkflowStep> WSteps = new();
+
+        public Workflow(WorkflowTemplate template, Guid candidateId, Guid invitingEID)
+        {
+            Name = template.Name;
+            Description = template.Description;
+            TemplateID = template.ID;
+            CandidateId = candidateId;
+            InvitingEID = invitingEID;
+            ID = Guid.NewGuid();
+        }
+
+        public void Approve(Employers employer, string feedback)
+        {
+            InvitingEID = employer.ID;
+            WSteps.Feedback = feedback;
+            WSteps.ModifiedDate = DateTime.Now;
+            Status = Position.Approved;
+        }
+
+        public void Reject(Employers employer, string feedback)
+        {
+            InvitingEID = employer.ID;
+            WSteps.Feedback = feedback;
+            WSteps.ModifiedDate = DateTime.Now;
+            Status = Position.Reject;
+        }
+
+        public void Restart(Employers employer)
+        {
+            InvitingEID = employer.ID;
+            WSteps.ModifiedDate = DateTime.Now;
+            Status = Position.InProgress;
+        }
+
+        public enum Position
+        {
+            Reject,
+            InProgress,
+            Approved
+        }
+    }
+}
---
+++ app/app/WorkflowTemplate.cs
@@ -0,0 +1,32 @@
+﻿using static app.Workflow;
+
+namespace app
+{
+    internal class WorkflowTemplate
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Guid ID { get; private set; }
+
+        public List<WorkflowStep> WSteps = new();
+
+        public WorkflowTemplate(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            ID = Guid.NewGuid();
+        }
+
+        public void Update()
+        {
+            WSteps.ModifiedDate = DateTime.Now;
+            bool isRejectSteps = WSteps.Where(x => x.Status == Position.Reject).Any();
+            Workflow.Status = isRejectSteps ? Position.Reject : Position.InProgress;
+        }
+
+        public Workflow Create(Guid candidateId, Guid invitingEID)
+        {
+            return null;
+        }
+    }
+}
